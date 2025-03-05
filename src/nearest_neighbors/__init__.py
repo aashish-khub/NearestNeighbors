852ef640050de989f1b227bf2a5e9bb717e51616
Aashish Khubchandani <ak7164@nyu.edu>
2025-03-04 19:10:07 -0500

    update with feedback from albert

--- src/nearest_neighbors/__init__.py
+++ src/nearest_neighbors/__init__.py
@@ -2,15 +2,17 @@
 # For example: `from nearest_neighbors import NearestNeighborImputer`
 # is easier to read than from `nearest_neighbors.nnimputer import NearestNeighborImputer`
 
+# TODO @ALL: please uncomment the following lines when the code is ready in each file
+
 from .dnn import *  # noqa: F403
 from .dnn_wasserstein import * # noqa: F403
-from .dnn_kernel import * # noqa: F403
-from .dr_nn import * # noqa: F403
-from .nadaraya_watson import * # noqa: F403
+# from .dnn_kernel import * # noqa: F403
+# from .dr_nn import * # noqa: F403
+# from .nadaraya_watson import * # noqa: F403 
 from .nnimputer import * # noqa: F403
-from .syn_nn import * # noqa: F403
-from .ts_nn import * # noqa: F403
-from .vanilla_nn import * # noqa: F403
+# from .syn_nn import * # noqa: F403
+# from .ts_nn import * # noqa: F403
+# from .vanilla_nn import * # noqa: F403
 from .utils import * # noqa: F403
 from .simulations import * # noqa: F403
-# add new files here
+# ...add new files here

aa7b497ef5b44f0249485d896b9755d602fa900c
Harshvardhan Maskara <163366934+harshvardhanmaskara@users.noreply.github.com>
2025-03-08 18:32:00 -0500

    added row-row and col-col imputers

--- src/nearest_neighbors/vanilla_nn.py
+++ src/nearest_neighbors/vanilla_nn.py
@@ -1,51 +1,34 @@
-"""Implementation of vanilla nearest neighbor imputation."""
-
-import numpy as np
-import numpy.typing as npt
-
-from .nnimputer import DataType, EstimationMethod
+from .nnimputer import NearestNeighborImputer
+from .data_types import Scalar
+from .estimation_methods import RowRowEstimator, ColColEstimator
+from typing import Optional
 
 
-class VectorDataType(DataType):
-    def __init__(self):
-        super().__init__()
+def row_row(distance_threshold: Optional[float] = None) -> NearestNeighborImputer:
+    """Create a row-row nearest neighbor imputer.
 
-    def distance(self, obj1: npt.NDArray, obj2: npt.NDArray) -> float:
-        """Compute the Euclidean distance between two vectors.
+    Args:
+        distance_threshold (float, optional): Distance threshold for nearest neighbors. Defaults to None.
 
-        Args:
-            obj1 (npt.NDArray): The first vector.
-            obj2 (npt.NDArray): The second vector.
+    Returns:
+        NearestNeighborImputer: A row-row nearest neighbor imputer.
 
-        Returns:
-            float: The Euclidean distance between the two vectors.
-
-        """
-        # assert that x is a vector
-        assert obj1.ndim == 1
-        # assert that y is a vector
-        assert obj2.ndim == 1
-        return float(np.linalg.norm(obj1 - obj2))
-
-    def average(self, object_list: npt.NDArray) -> float:
-        """Compute the average of a vector.
-
-        Args:
-            object_list (npt.NDArray): A list of vectors.
-
-        Returns:
-            float: The average of the vector.
-
-        """
-        assert object_list.ndim == 2
-        return np.mean(object_list, axis=0)
+    """
+    estimator = RowRowEstimator()
+    data_type = Scalar()
+    return NearestNeighborImputer(estimator, data_type, distance_threshold)
 
 
-class RowRowEstimator(EstimationMethod):
-    pass
+def col_col(distance_threshold: Optional[float] = None) -> NearestNeighborImputer:
+    """Create a column-column nearest neighbor imputer.
 
+    Args:
+        distance_threshold (float, optional): Distance threshold for nearest neighbors. Defaults to None.
 
-class ColumnColumnEstimator(RowRowEstimator):
-    # TODO: this is the same as RowRowEstimator
-    # except the matrix is transposed
-    pass
+    Returns:
+        NearestNeighborImputer: A column-column nearest neighbor imputer.
+
+    """
+    estimator = ColColEstimator()
+    data_type = Scalar()
+    return NearestNeighborImputer(estimator, data_type, distance_threshold)

40a2c14b3eb74fe2149ba8eb5ac46d347099510f
Albert Gong <ag2435@cornell.edu>
2025-04-15 21:05:06 -0400

    make helper functions static so that they can be cached using joblib.Memory

--- src/nearest_neighbors/datasets/dataloader_base.py
+++ src/nearest_neighbors/datasets/dataloader_base.py
@@ -2,7 +2,6 @@
 
 from abc import ABC, abstractmethod
 import numpy as np
-import os
 from typing import Any
 
 
@@ -13,8 +12,6 @@ class NNDataLoader(ABC):
 
     def __init__(
         self,
-        download: bool = False,
-        save_dir: str = "./",
         agg: str = "mean",
         save_processed: bool = False,
         **kwargs: Any,
@@ -22,19 +19,11 @@ class NNDataLoader(ABC):
         """Initializes the data loader.
 
         Args:
-            download (bool): whether to download the data locally. Default: False. If True, data is downloaded at save_dir
-            save_dir (str): directory to download the data to or where it already exists. Also the directory where the processed data will be.
-                Default: "./" (current directory).
             agg (str): aggregation method to use to create scalar dataset. Default: "mean".
             save_processed (bool): whether to save the processed data.  Default: False.
             **kwargs: additional arguments to be passed to the subclass.
 
         """
-        self.save_dir = save_dir
-        if not os.path.isdir(save_dir):
-            print("Save directory not found. Creating it.")
-            os.makedirs(save_dir)
-        self.download = download
         self.save_processed = save_processed
         if agg not in self.supported_aggs:
             raise ValueError(
@@ -42,35 +31,30 @@ class NNDataLoader(ABC):
             )
         self.agg = agg
 
-        if download:
-            # this downloads the data to save_dir .
-            self.download_data()
+    # NOTE: use joblib.Memory to cache the data instead of downloading the data
+    # @abstractmethod
+    # def download_data(self) -> None:
+    #     """Downloads the data to self.save_dir."""
+    #     raise NotImplementedError
 
     @abstractmethod
-    def download_data(self) -> None:
-        """Downloads the data to self.save_dir."""
-        raise NotImplementedError
-
-    @abstractmethod
-    def process_data_scalar(
-        self, cached: bool = False
-    ) -> tuple[np.ndarray, np.ndarray]:
+    def process_data_scalar(self) -> tuple[np.ndarray, np.ndarray]:
         """Processes the data into scalar matrix setting. Returns a tuple of ndarrays representing 2d data matrix and mask.
 
-        Args:
-            cached (bool): whether to use cached data. If False, then directly use urls, otherwise use self.save_dir. Default: False.
+        Returns:
+            data: 2d data matrix
+            mask: 2d mask matrix
 
         """
         raise NotImplementedError
 
     @abstractmethod
-    def process_data_distribution(
-        self, cached: bool = False
-    ) -> tuple[np.ndarray, np.ndarray]:
+    def process_data_distribution(self) -> tuple[np.ndarray, np.ndarray]:
         """Processes the data into distributional setting. Returns a tuple of ndarrays representing 4d distributional data and mask.
 
-        Args:
-            cached (bool): whether to use cached data. If False, then directly use urls, otherwise use self.save_dir. Default: False.
+        Returns:
+            data: 4d distributional data matrix
+            mask: 2d mask matrix
 
         """
         raise NotImplementedError

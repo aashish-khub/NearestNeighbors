450680b2237fe3cfd8ac8430aecbfd6d6173a988
Albert Gong <ag2435@cornell.edu>
2025-05-02 20:06:35 -0400

    changed figure width to be exactly 0.4\textwidth of neurips format

---
+++ src/nearest_neighbors/utils/plotting_utils.py
@@ -0,0 +1,27 @@
+# Enable LaTeX for text rendering
+import matplotlib as mpl
+
+mpl.rcParams["text.latex.preamble"] = r"\usepackage{amssymb} \usepackage{amsmath}"
+mpl.rcParams["text.usetex"] = True
+mpl.rcParams["font.family"] = "serif"
+
+
+# Single column figures
+TICK_FONT_SIZE = 6
+MINOR_TICK_FONT_SIZE = 3
+LABEL_FONT_SIZE = 10
+LEGEND_FONT_SIZE = 7
+MARKER_ALPHA = 1
+MARKER_SIZE = 3
+LINE_ALPHA = 0.75
+OUTWARD = 4
+
+METHOD_ALIASES = {
+    "row-row": "Row-NN",
+    "col-col": "Col-NN",
+    "dr": "DR-NN",
+    "ts": "TS-NN",
+    "usvt": "USVT",
+    "softimpute": "SoftImpute",
+    "nadaraya": "NW",
+}

6924754f18a1e5ab10f0addda9f3ad76e65fd318
Albert Gong <ag2435@cornell.edu>
2025-05-05 23:35:40 -0400

    added script to plot synthetic controls

---
+++ examples/prop99/plot_synthetic_control.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from glob import glob
+import os
+import logging
+
+from nearest_neighbors.utils.experiments import get_base_parser
+from nearest_neighbors.utils import plotting_utils
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+parser = get_base_parser()
+args = parser.parse_args()
+output_dir = args.output_dir
+california_dir = os.path.join(output_dir, "california")
+figures_dir = os.path.join(output_dir, "figures")
+os.makedirs(figures_dir, exist_ok=True)
+
+files = glob(os.path.join(california_dir, "california-*.csv"))
+df_list = []
+
+fig = plt.figure(figsize=(5.5, 3))
+ax = fig.add_subplot(111)
+for i, file in enumerate(files):
+    df = pd.read_csv(file, index_col=0)
+    method = df.iloc[0]["estimation_method"]
+    ax.plot(df.index, df["control"], label=f"Control ({method})", linestyle="--")
+    if i == 0:
+        ax.plot(df.index, df["obs"], label="Observed")
+# add a vertical line at 1989 called Proposal 99
+ax.axvline(x=1989, color="k", alpha=0.25)
+# add a text label at 1989 called Proposal 99
+ax.text(1989, ax.get_ylim()[1], "Proposal 99", color="k", fontsize=12)
+# set the xtick labels to be the years 1970 to 2000
+ax.set_xticks(
+    range(1970, 2001, 10),
+    [str(year) for year in range(1970, 2001, 10)],
+    fontsize=plotting_utils.TICK_FONT_SIZE,
+)
+ax.set_xlim(1970, 2000)
+ax.set_ylim(40, 160)
+# set the y-axis label to be the number of cigarettes smoked per capita
+ax.set_ylabel(
+    "Cigarette Consumption\n(Pack Sales Per Capita)",
+    fontsize=plotting_utils.LABEL_FONT_SIZE,
+)
+# set the x-axis label to be the year
+ax.set_xlabel("Year", fontsize=plotting_utils.LABEL_FONT_SIZE)
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["bottom"].set_position(
+    ("outward", plotting_utils.OUTWARD)
+)  # Move x-axis outward
+ax.spines["left"].set_position(
+    ("outward", plotting_utils.OUTWARD)
+)  # Move y-axis outward
+ax.legend()
+# plt.show()
+plt.savefig(os.path.join(figures_dir, "california-synthetic-control.pdf"))

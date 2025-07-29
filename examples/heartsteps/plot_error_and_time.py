663eb7761804140b07940ae7a756e9903a654ed3
Albert Gong <ag2435@cornell.edu>
2025-07-29 15:48:07 -0400

    print mean and quantiles as markdown table

--- examples/heartsteps/plot_error_and_time.py
+++ examples/heartsteps/plot_error_and_time.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
 from glob import glob
 import pandas as pd
 import logging
+import numpy as np
+from tabulate import tabulate
 
 from nsquared.utils.experiments import get_base_parser
 from nsquared.utils import plotting_utils
@@ -60,6 +62,35 @@ ORDER = [
 ]
 df_grouped = df_grouped.sort_values(
     by="estimation_method", key=lambda x: x.map(lambda y: ORDER.index(y))
+)
+# Compute mean, 25%, 50%, 75% quantiles for each method
+df_grouped["mean_error"] = df_grouped["est_errors"].apply(
+    lambda x: np.mean(x) if len(x) > 0 else np.nan
+)
+df_grouped["median_error"] = df_grouped["est_errors"].apply(
+    lambda x: np.quantile(x, 0.5) if len(x) > 0 else np.nan
+)
+df_grouped["25%_error"] = df_grouped["est_errors"].apply(
+    lambda x: np.quantile(x, 0.25) if len(x) > 0 else np.nan
+)
+df_grouped["75%_error"] = df_grouped["est_errors"].apply(
+    lambda x: np.quantile(x, 0.75) if len(x) > 0 else np.nan
+)
+print(
+    tabulate(
+        df_grouped[
+            [
+                "estimation_method",
+                "mean_error",
+                "median_error",
+                "25%_error",
+                "75%_error",
+            ]
+        ],
+        headers="keys",
+        tablefmt="github",
+        showindex=False,
+    )
 )
 
 for col_name, alias in [
